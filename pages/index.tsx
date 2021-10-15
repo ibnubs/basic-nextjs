f8b1b83bfbb272c993efaedf9f86c1d0269a8c08
ibnubs <ibnubs@gmail.com>
2021-10-16 02:54:25 +0700

    learn add image NextJS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout';
 import style from '../styles/Home.module.css';
+import Image from 'next/image';
 
 export default function Home() {
   return (
       <Layout pageTitle="Home Page">
+        <Image src="/profile.jpg" width={200} height={200} alt="profile" />
         <h1 className={style['title-homepage']}>Home Page</h1>
       </Layout>
 
